df28720a751fe26cfc7cab963690837def6ee5f7
Adam DePaolo <amdepaolo@gmail.com>
2023-04-24 17:14:31 -0400

    adds update and destroy to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,19 @@ class UsersController < ApplicationController
         render json: {errors: invalid.record.errors}, status: :unprocessable_entity
     end
 
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user, status: :accepted
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        session.delete :user_id
+        head :no_content
+    end
+
     private
 
     def user_params
