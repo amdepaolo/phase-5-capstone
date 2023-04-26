2ac609bf53682bcb437004a9b0b634eeba56a290
Adam DePaolo <amdepaolo@gmail.com>
2023-04-26 18:55:21 -0400

    creates QuestionChannel for action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+  mount ActionCable.server => '/cable'
+
+  get '/doathing', to: 'application#doathing'
+  
   get '/me', to: 'users#show'
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
-  post '/users', to: 'users#create'
-
+  resources :users, only: [:create, :update, :destroy]
   resources :games, only: [:index, :show, :create] do
     resources :players, only: [:create, :destroy]
     resources :questions, only: [:create] do 
